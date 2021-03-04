896a6eedeae501e946a62a6ad9aa4c2c8c690849
Nate Palmer <nate@natedpalm.com>
2021-03-04 12:42:09 -0500

    Update kraken, add in pretty please alias

--- src/kraken.sh
+++ src/kraken.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 liquidctl initialize all
-liquidctl set sync color spectrum-wave --pick 0 # pick 0 for case lights
+liquidctl set sync color super-rainbow --speed slowest --pick 0 # pick 0 for case lights
 liquidctl set fan1 speed 100 --pick 0
 liquidctl set fan2 speed 100 --pick 0
 liquidctl set fan3 speed 100 --pick 0
 
-liquidctl set ring color spectrum-wave --pick 1 # pick 1 for kraken coolor
-liquidctl set logo color spectrum-wave --pick 1 
+liquidctl set ring color super-rainbow --speed slowest  --pick 1 # pick 1 for kraken coolor
+liquidctl set logo color super-rainbow --speed slowest  --pick 1 
 liquidctl set pump speed 100 --pick 1
